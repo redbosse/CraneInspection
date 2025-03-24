897771315998c26a3e8b54a09bdb99ff78e787bf
Sergiychuk Viachaslau <48277320+redbosse@users.noreply.github.com>
2025-03-24 16:27:41 +0300

    Build Scene

--- Assets/Scripts/Questions/Answer.cs
+++ Assets/Scripts/Questions/Answer.cs
@@ -8,8 +8,12 @@ namespace Questions
     {
         [SerializeField] private string answer;
         [SerializeField] private bool correct;
+        [SerializeField] private int scoreCorrect;
+        [SerializeField] private int scoreUncorrect;
         
         public string AnswerString => answer;
         public bool Correct => correct;
+        public int ScoreCorrect => scoreCorrect;
+        public int ScoreUncorrect => scoreUncorrect;
     }
 }
